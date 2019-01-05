f14ff7d729061ca9fee05df2080c82da2b253718
henrik <hlammert@hotmail.de>
2019-01-05 14:48:20 +0100

    Added: Error Meldung, wenn kein Favorit ausgewählt wurde

--- app/src/main/java/com/example/hlamm/bkolsberg/DisplayFavoritesActivity.java
+++ app/src/main/java/com/example/hlamm/bkolsberg/DisplayFavoritesActivity.java
@@ -1,11 +1,13 @@
 package com.example.hlamm.bkolsberg;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -16,6 +18,7 @@ public class DisplayFavoritesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean favorites_exist = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_alle_bildungsgaenge);
 
@@ -23,7 +26,10 @@ public class DisplayFavoritesActivity extends AppCompatActivity {
 
         for(int i = 0; i < bildungsgaenge.size(); i++) {
             //Selecting just Favorites
-            if(bildungsgaenge.get(i).isFavorit()) cardItems.add(new CardItem(bildungsgaenge.get(i).getId(), bildungsgaenge.get(i).getBezeichnung(), bildungsgaenge.get(i).getDauer(), bildungsgaenge.get(i).isFavorit()));
+            if(bildungsgaenge.get(i).isFavorit()) {
+                cardItems.add(new CardItem(bildungsgaenge.get(i).getId(), bildungsgaenge.get(i).getBezeichnung(), bildungsgaenge.get(i).getDauer(), bildungsgaenge.get(i).isFavorit()));
+                favorites_exist = true;
+            }
         }
 
 
@@ -34,6 +40,8 @@ public class DisplayFavoritesActivity extends AppCompatActivity {
         recyclerView.setAdapter(cardItemAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        //Error-Message if user did not chose favorites
+        if(!favorites_exist) Toast.makeText(getApplicationContext(), getString(R.string.error_no_favorites), Toast.LENGTH_LONG).show();
     }
 
     /**
